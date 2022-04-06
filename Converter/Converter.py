91643f207a31d2b926442492b8142a15e347092c
mhtvsSFrpHdE <2bemperor47@gmail.com>
2022-04-06 08:32:30 +0800

    Color under formula environment

--- Converter/Converter.py
+++ Converter/Converter.py
@@ -1,14 +1,30 @@
 import sys
 
 if len(sys.argv) == 1:
-	print("No file to open")
-	sys.exit()
+    print("No file to open")
+    sys.exit()
 
 filePath = sys.argv[1]
 
 # https://stackoverflow.com/questions/13089234/replacing-text-in-a-file-with-python
 
-replacements = {'openFont':'<font', 'closeFont':'>', 'endFont':'</font>'}
+replacements = {
+    # HTML
+
+    # span
+    '/openSpan/': '<span',
+    '/closeSpan/': '>',
+    '/endSpan/': '</span>',
+
+    # Text style
+
+    # color
+    '/beginTextcolor/': r'\textcolor',
+    '/hashSign/': '#',
+
+    # bold
+    '/beginBold/': r'\textbf'
+}
 
 lines = []
 
